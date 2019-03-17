257e8e8353cde9ee34d8c2d3cc13e54cc35d0aac
Ultraguy55 <ultraguy55@gmail.com>
2019-03-17 00:05:08 -0400

    Added URL saving of configured particle spawnrer

--- FireGen/fire.js
+++ FireGen/fire.js
@@ -12,6 +12,7 @@ var spawnerAddMode = false;
 var GUIArr = [];
 var customMode = false;
 var customSpawners = [];
+var shape = 1;
 class Particle {
   constructor(x, y) {
     this.x = x;
@@ -59,10 +60,12 @@ class Particle {
   show() {
     noStroke();
     fill(this.col.x, this.col.y, this.col.z);
-    if (shape.value() == 1 || !shape.value()) {
+    if (shape == 1) {
       ellipse(this.x, this.y, this.size, this.size);
-    } else if (shape.value() == 2) {
+    } else if (shape == 2) {
       rect(this.x, this.y, this.size, this.size);
+    } else {
+      ellipse(this.x, this.y, this.size, this.size);
     }
   }
 
@@ -76,6 +79,19 @@ function setup() {
   createGUI();
   currentPos = createVector(width / 2, height / 2)
   spawners = generateSpawners(4);
+  var data = getURLParams();
+  speed.value(data.speed || speed.value());
+  color1r.value(data.c1r || color1r.value());
+  color1g.value(data.c1g || color1g.value());
+  color1b.value(data.c1b || color1b.value());
+  color2r.value(data.c2r || color2r.value());
+  color2g.value(data.c2g || color2g.value());
+  color2b.value(data.c2b || color2b.value());
+  spawnerAmount.value(data.spawn || spawnerAmount.value())
+  lifeSpan.value(data.lifeSpan || lifeSpan.value())
+  shape = data.shape || shapeRadio.value();
+  spawnerDistance.value(data.radius || spawnerDistance.value())
+
 
 }
 
@@ -85,54 +101,61 @@ function createGUI() {
   GUIArr.push(speed);
   GUIArr.push(createP("Spawner Rotation Speed").position(0, space * 0).style("color", "white"))
   speed.position(0, space * 1);
-
+  speed.changed(updateURL)
   //First Color
   color1r = createSlider(0, 255, 255, 1);
   GUIArr.push(color1r)
   GUIArr.push(createP("First Color Red").position(0, space * 2).style("color", "white"))
   color1r.position(0, space * 3);
+  color1r.changed(updateURL)
 
   color1g = createSlider(0, 255, 221, 1);
   GUIArr.push(color1g);
   GUIArr.push(createP("First Color Green").position(0, space * 4).style("color", "white"));
   color1g.position(0, space * 5);
+  color1g.changed(updateURL)
 
   color1b = createSlider(0, 255, 0, 1);
   GUIArr.push(color1b);
   GUIArr.push(createP("First Color Blue").position(0, space * 6).style("color", "white"));
   color1b.position(0, space * 7);
-
+  color1b.changed(updateURL)
   //Second Color
   color2r = createSlider(0, 255, 255, 1);
   GUIArr.push(color2r);
   GUIArr.push(createP("Second Color Red").position(0, space * 8).style("color", "white"))
   color2r.position(0, space * 9);
+  color2r.changed(updateURL)
 
   color2g = createSlider(0, 255, 0, 1);
   GUIArr.push(color2g);
   GUIArr.push(createP("Second Color Green").position(0, space * 10).style("color", "white"));
   color2g.position(0, space * 11);
+  color2g.changed(updateURL)
 
   color2b = createSlider(0, 255, 0, 1);
   GUIArr.push(color2b);
   GUIArr.push(createP("Second Color Blue").position(0, space * 12).style("color", "white"));
   color2b.position(0, space * 13);
+  color2b.changed(updateURL)
 
   spawnerAmount = createSlider(0, 10, 4, 1);
   GUIArr.push(spawnerAmount);
   GUIArr.push(createP("Amount of Spawners").position(0, space * 14).style("color", "white"));
   spawnerAmount.position(0, space * 15);
+  spawnerAmount.changed(updateURL)
 
   lifeSpan = createSlider(1, 300, 200, 1);
   GUIArr.push(lifeSpan);
   GUIArr.push(createP("Life Span of Particles").position(0, space * 16).style("color", "white"));
   lifeSpan.position(0, space * 17);
-
+  lifeSpan.changed(updateURL)
 
   spawnerDistance = createSlider(1, 500, 100, 1);
   GUIArr.push(spawnerDistance);
   GUIArr.push(createP("Radius of Spawners").position(0, space * 18).style("color", "white"));
   spawnerDistance.position(0, space * 19);
+  spawnerDistance.changed(updateURL)
 
   rainbowBox = createCheckbox("Rainbow?", false);
   GUIArr.push(rainbowBox);
@@ -140,16 +163,22 @@ function createGUI() {
   rainbowBox.style("color", "white");
   rainbowBox.changed((function () {
     rainbow = !rainbow;
+    updateURL();
   }))
 
-  shape = createRadio();
-  GUIArr.push(shape);
+  shapeRadio = createRadio();
+  GUIArr.push(shapeRadio);
   GUIArr.push(createP("Shape?").position(150, space * 3).style("color", "white"));
-  shape.option("Circle", 1);
-  shape.option("Sqaure", 2);
-  shape.style("color", "white");
-  shape.position(150, space * 4);
-
+  shapeRadio.option("Circle", 1);
+  shapeRadio.option("Sqaure", 2);
+  shapeRadio.style("color", "white");
+  shapeRadio.position(150, space * 4);
+  function updateShape() {
+    shape = shapeRadio.value();
+    updateURL();
+  }
+  shapeRadio.changed(updateShape);
+  
 
   randomizer = createButton("Randomize Values")
   GUIArr.push(randomizer);
@@ -197,8 +226,11 @@ function randomizeValues() {
   spawnerAmount.value(random(0, 10));
   lifeSpan.value(random(1, 300));
   spawnerDistance.value(random(1, 500));
+  updateURL();
 }
-
+function updateURL() {
+  window.history.replaceState( {} , 'Particle Generator', `?speed=${speed.value()}&c1r=${color1r.value()}&c1g=${color1g.value()}&c1b=${color1b.value()}&c2r=${color2r.value()}&c2g=${color2g.value()}&c2b=${color2b.value()}&spawn=${spawnerAmount.value()}&lifeSpan=${lifeSpan.value()}&shape=${shape}&radius=${spawnerDistance.value()}` );
+}
 function draw() {
   colorMode(RGB);
   background(30);
@@ -262,6 +294,7 @@ function draw() {
 
   }
 
+  
 
 }
 
